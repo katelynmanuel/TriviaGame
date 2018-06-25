7328b3954bfd778eff9f24efb599134fe9803cc7
Katelyn Manuel <kmanuel@osc-laptop-44.osmosissc.com>
2018-06-24 21:08:36 -0600

    Adding latest changes to app.js as well as the latest changes to the html and css files.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # TriviaGame
-Unit 5 Trivia Assignment
+Assigment Five: Trivia Game.
+
+### Overview
+
+Harry Potter Trivia! In this game, the user is shown 10 trivia questions and given 20 seconds to answer the question. If the timer runs out, the player is notified and the next question is displayed. At the end of the game, the player gets a summary of the results of their correct, incorrect, and unansweered questions. 
+
+### Step One
+
+First, I created variables and an array with objects to hold the question, choices, and correct answer values by pulling the index of the correct answer from the choices object.
+
+### Step Two
+
+Next, I created a start button that would display to the page allowing the user to begin the game when ready. 
+
+### Step Three
+
+When the Start button clicked, I created a function that would clear the directions and start button and display the first question and the choices.
+
+### Step Four
+
+In this step, I created another function that would check to see if we have gone through the entirety of the trivia questions variable to determine whether or not we would display another question or show the user their total scores. 
+
+### Step Five 
+
+My next step was to create a function that would allow us to check to see if the player's answer was correct or inccorrect and then inserted this function in the above step so that the answer is checked and then the function to either display the next question or player results is run.
+
+### Step Six
+
+In this step, I created the timer that is set at 20 seconds, goes down by one second, stops at zero, and displays the time left to user on the DOM. 
+
+### Step Seven
+
+This last step is a function that would allow the user to play the game again. I cretaed a button that the player can click on to restart the game. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,189 @@
-$(document).ready()
-
-//Create Global Variables
-var time = 120;
+//Create variables
+var i = 0;
 var intervalId = "";
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unansweredQuestions = 0;
+var answerChoices = 0;
+var values = "";
+var time = 30;
 
-var questionOne = {
-    question: "What is the name of the house Harry Potter gets sorted into?",
-    choices: ["Ravenclaw", "Slytherin", "Gryffindor", "Hufflepuff"],
-    correct: ["Gryffindor"]
-};
+$(document).ready(function() {
 
-var questionTwo = {
-    question: "Which one of these is NOT part of the Deathly Hallows?",
-    choices: ["Ressurection Stone", "Elder Wand", "Sorcerer's Stone", "Cloak of Invisibility"],
-    correct: ["Sorcerer's Stone"]
-};
+//Create Trivia Questions
+var triviaQuestions = [
+    {
+        question: "What is the name of the house Harry Potter gets sorted into?",
+        choices: ["Ravenclaw", "Slytherin", "Gryffindor", "Hufflepuff"],
+        value: 2,
+        image: ["src", "assets/images/GryffindorCrest.jpg"],
+        alt: "Gryffindor"
+    },
 
-var questionThree = {
-    question: "What is the name of the three-headed dog protecting the trap door in Harry Potter and the Sorcerer's Stone?",
-    choices: ["Fang", "Scabbers", "Aragog", "Fluffy"],
-    correct: ["Fluffy"]
-};
+    {
+        question: "Which one of these is NOT part of the Deathly Hallows?",
+        choices: ["Ressurection Stone", "Elder Wand", "Sorcerer's Stone", "Cloak of Invisibility"],
+        value: 2,
+    },
 
-var questionFour = {
-    question: "How many siblings does Ron Weasley have?",
-    choices: ["Six", "Four", "Twelve", "Three"],
-    correct: ["Six"]
-};
+    {
+        question: "What is the name of the three-headed dog protecting the trap door in Harry Potter and the Sorcerer's Stone?",
+        choices: ["Fang", "Fluffy", "Aragog", "Scabbers"],
+        value: 2,
+    },
 
-var questionFive = {
-    question: "What is the name of the professor that teaches Potions at Howgwarts?",
-    choices: ["Minerva McGonagall", "Severus Snape", "Sybill Trewlaney", "Albus Dubmledore"],
-    correct: ["Severus Snape"]
-};
+    {
+        question: "How many siblings does Ron Weasley have?",
+        choices: ["Six", "Four", "Twelve", "Three"],
+        value: 0,
+    },
+
+    {
+        question: "What is the name of the professor that teaches Potions at Hogwarts?",
+        choices: ["Minerva McGonagall", "Severus Snape", "Sybill Trewlaney", "Albus Dubmledore"],
+        value: 1,
+    },
+
+    {
+        question: "What shape does Harry's patronus take?",
+        choices: ["an Otter", "a Dog", "a Stag", "a Dragon"],
+        value: 2,
+    },
+
+    {
+        question: "What is Hermione's middle name?",
+        choices: ["Jean", "Marie", "Anne", "Lily"],
+        value: 0,
+    },
+
+    {
+        question: "Who is the Keeper of Keys and Grounds at Hogwarts?",
+        choices: ["Gilderoy Lockhart", "Rubeus Hagrid", "Argus Filch", "Albus Dumbledore"],
+        value: 1,
+    },
+
+    {
+        question: "What is the name of the potion that's also called Liquid Luck?",
+        choices: ["Veritaserum", "Amortentia", "Felix Felicis", "Polyjuice POtion"],
+        value: 2,
+    },
+
+    {
+        question: "Which newspaper did Rita Skeeter work for?",
+        choices: ["The Daily Prophet", "The Quibbler", "The Wizard's Voice", "Wizard People"],
+        value: 0,
+    },
+]
 
 //Function to start game with Start button
-function startButton () {
+function startButton() {
+    $(".displayChoices").hide();
     var startQuestionsButton = $("<button>");
-    startQuestionsButton.text("Start the Game!");
-    $(".displayButton").html(startQuestionsButton);
-    console.log(startQuestionsButton);
+    startQuestionsButton.text("Alohamora");
+    $(".displayButton").append(startQuestionsButton);
 };
 
 startButton();
 
-//Create function to start timer when user clicks on Start button
-function startInterval() {
-    intervalId = setInterval(decrement, 120000);
+//Displays current question when start button is clicked.
+$(".displayButton").on("click", function displayCurrentQuestion () {
+    run();
+    $(".main").empty();
+    $(".displayButton").empty();
+    $(".displayChoices").show();
+    var currentQuestion = `
+    <h4>${triviaQuestions[i].question}</h4><br/>
+    <input type="radio" name="choices" value="0">${triviaQuestions[i].choices[0]}</input><br/>
+    <input type="radio" name="choices" value="1">${triviaQuestions[i].choices[1]}</input><br/>
+    <input type="radio" name="choices" value="2">${triviaQuestions[i].choices[2]}</input><br/>
+    <input type="radio" name="choices" value="3">${triviaQuestions[i].choices[3]}</input><br/><br/>
+    `;
+    $(".displayQuestion").html(currentQuestion);
+    console.log(currentQuestion);
+});
+
+//Check to see if there are more questions. If i > 9, display user results, else move to current question.
+$("#submit").on("click", function() {
+    var prevQuestion = triviaQuestions[i];
+    checkAnswer(prevQuestion);
+    i++
+    if (i > 9) {
+        stop();
+        $(".displayChoices").empty();
+        $(".displayQuestion").empty();
+        $(".displayTimer").hide();
+        $(".displayScores").html(`
+        <h3>Your Results</h3>
+        <h4>Correct: ${correctAnswers}</h3>
+        <h4>Incorrect: ${incorrectAnswers}</h4>
+        <h4>Unanswered: ${unansweredQuestions}</h4>
+        `)
+        playAgain();
+    } else { 
+        var currentQuestion = `
+        <h4>${triviaQuestions[i].question}</h4><br/>
+        <input type="radio" name="choices" value="0">${triviaQuestions[i].choices[0]}</input><br/>
+        <input type="radio" name="choices" value="1">${triviaQuestions[i].choices[1]}</input><br/>
+        <input type="radio" name="choices" value="2">${triviaQuestions[i].choices[2]}</input><br/>
+        <input type="radio" name="choices "value="3">${triviaQuestions[i].choices[3]}</input><br/><br/>
+        `;
+        $(".displayQuestion").html(currentQuestion);
+    }
+    stop();
+    run();
+});
+
+//Function to check if answer is correct or incorrect.
+function checkAnswer (previousQuestion) {
+    var checked = $("input:radio[name=choices]:checked").val();
+    console.log("User Input: " + checked);
+    if (checked == previousQuestion.value) {
+        correctAnswers++;
+        console.log("Correct: " + correctAnswers);
+        console.log("Correct");
+    } else {
+        incorrectAnswers++;  
+    }
+    stop();
+    run();
 };
 
-//Create decrement function and display to the page.
-function decrement() {
-    time--;
-    $(".displayTimer").html("Time remaining: " + time + " seconds.");
-}
+//Function to start timer and display to html page.
+function run () {
+    clearInterval(intervalId);
+    time = 21;
+    intervalId = setInterval(decrement, 1000);
+    function decrement() {
+        time--;
+        $(".displayTimer").html("<br/><br/><h4>Time Remaining: " + time + " seconds.</h4>");
+        if (time === 0){
+            stop();
+            $(".displayTimer").html("<h4>Time's Up!</h4>");
+            unansweredQuestions++
+            checkAnswer();
+            $(".displayQuestion").hide();
+        }
+    };
 
-//Display questions and possible answers to the page. Then display whether or not the user was correct. 
+};
 
-//Display the total number of quesitons answered correctly versus incorrectly and unanswered. 
+function stop() {
+    clearInterval(intervalId);
+}; 
+
 
 //Display button if the user wants to play again. 
+function playAgain () {
+    $(".displayChoices").empty();
+    $(".displayQuestion").empty();
+    $(".displayTimer").hide();
+    var playAgainButton = $("<button>");
+    playAgainButton.text("Play Again!");
+    $(".displayPlayAgain").html(playAgainButton);
+
+    $(".displayPlayAgain").on("click", function nextRound () {
+    displayCurrentQuestion();
+    });
+};
+
+});
